085f9e88ac9e8e6fd1d92243028dfc4c3fa4ca29
HelenPetukhova <lekar21@yahoo.com>
2025-09-11 01:15:14 +0300

    Test

---
+++ src/main/java/com/JavaTraining/App.java
@@ -0,0 +1,13 @@
+package com.JavaTraining;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
